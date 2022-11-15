9baa295f82a05eb56e1f6388925a9f04ce63ddf2
Luisfer Guevara <luisferguevaraa@gmail.com>
2022-11-15 11:50:09 +0100

    review

--- src/api/index/index.routes.js
+++ src/api/index/index.routes.js
@@ -3,7 +3,7 @@ const express = require("express");
 const router = express.Router();
 
 router.get("/", (req, res) => {
-  return res.status(200).json("SERVIDOR OK, BIENVENIDXS A NUESTRA GALERÍA RENACENTISTA");
+  return res.status(200).json("SERVIDOR OK, BIENVENIDXS");
 });
 
 module.exports = router;
